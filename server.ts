e6e198271f0ad07055beca1d6c3d7f1eb62fa097
n1pu <nipudos-01@yahoo.com>
2023-06-07 13:11:00 +0200

    use top-level await (breaking)

--- server.ts
+++ server.ts
@@ -1,15 +1,36 @@
-import { Server, ServerCredentials } from "@grpc/grpc-js";
-import { MassaService } from "./proto/massa/api/v1/api";
+import { credentials } from "@grpc/grpc-js";
+import { MassaServiceClient } from "./build/nodejs/api_grpc_pb";
+import { GetVersionRequest, GetVersionResponse } from "./build/nodejs/api_pb";
 
 const port = process.env.PORT || 33037;
 const host = process.env.HOST || "149.202.84.7";
 const url = `${host}:${port}`.replace(" ", "");
 console.log(url);
 
-const server = new Server();
-server.bindAsync(url, ServerCredentials.createInsecure(), () => {
-    server.start();
-    MassaService;
+const service = new MassaServiceClient(url, credentials.createInsecure());
+// service.getVersion(new GetVersionRequest(), (err, res) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(res);
+//     }
+// });
 
-    console.log(`server is running on ${url}`);
-});
+const getVersion = async (): Promise<GetVersionResponse> => {
+    return new Promise((resolve, reject) => {
+        service.getVersion(new GetVersionRequest(), (err, version) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(version);
+        });
+    });
+};
+
+await getVersion()
+    .then((version) => {
+        console.log(version);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
