c10c5c8ba8e7deee6209572b5fe29f3d63581ec5
n1pu <nipudos-01@yahoo.com>
2023-06-08 16:19:20 +0200

    subsribe to filled blocks

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
-import { credentials } from "@grpc/grpc-js";
+import { ClientDuplexStream, credentials } from "@grpc/grpc-js";
 import { MassaServiceClient } from "./build/nodejs/api_grpc_pb";
 import {
-    GetBlocksRequest,
-    GetBlocksResponse,
     GetVersionRequest,
     GetVersionResponse,
-    NewBlocksResponse,
+    NewFilledBlocksResponse,
+    NewOperationsResponse,
+    NewSlotExecutionOutputsResponse,
 } from "./build/nodejs/api_pb";
+import { TransactionsThroughputResponse } from "./build/nodejs/api_pb";
 
 const port = process.env.PORT || 33037;
-const host = process.env.HOST || "149.202.84.7";
+const host = process.env.HOST || "37.187.156.118";
 const url = `${host}:${port}`.replace(" ", "");
 console.log(url);
-
 const service = new MassaServiceClient(url, credentials.createInsecure());
-// service.getVersion(new GetVersionRequest(), (err, res) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(res);
-//     }
-// });
 
 const getVersion = async (): Promise<GetVersionResponse> => {
     return new Promise((resolve, reject) => {
@@ -33,21 +26,87 @@ const getVersion = async (): Promise<GetVersionResponse> => {
     });
 };
 
-const getBlocks = async (): Promise<GetBlocksResponse> => {
+const subscribeNewOperations = async () => {
+    const stream = service.newOperations();
     return new Promise((resolve, reject) => {
-        service.getBlocks(new GetBlocksRequest(), (err, version) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(version);
+        stream.on("data", (data: NewOperationsResponse) => {
+            console.log(data.toObject());
+        });
+        stream.on("error", (err) => {
+            console.log(err);
+            reject(err);
+        });
+        stream.on("end", (e: any) => {
+            console.log("end");
+            resolve(e);
         });
     });
 };
 
-const subscribe = async () => {
-    const stream = service.newBlocks();
+const subscribeTransactionsThroughput = async () => {
+    const stream = service.transactionsThroughput();
     return new Promise((resolve, reject) => {
-        stream.on("data", (data: NewBlocksResponse) => {
+        stream.on("data", (data: TransactionsThroughputResponse) => {
+            console.log(data.toObject());
+        });
+        stream.on("error", (err) => {
+            console.log(err);
+            reject(err);
+        });
+        stream.on("end", (e: any) => {
+            console.log("end");
+            resolve(e);
+        });
+    });
+};
+
+const subscribeNewSlotExecutionOutputs = async () => {
+    const stream = service.newSlotExecutionOutputs();
+    return new Promise((resolve, reject) => {
+        stream.on("data", (data: NewSlotExecutionOutputsResponse) => {
+            console.log(data.toObject());
+        });
+        stream.on("error", (err) => {
+            console.log(err);
+            reject(err);
+        });
+        stream.on("end", (e: any) => {
+            console.log("end");
+            resolve(e);
+        });
+    });
+};
+
+const subscribeFilledBlocks = async () => {
+    const stream = service.newFilledBlocks();
+    return new Promise((resolve, reject) => {
+        stream.on("data", (data: NewFilledBlocksResponse) => {
+            const block = data.getFilledBlock()?.toObject();
+            const operations = block?.operationsList;
+            console.log(block?.header?.id, operations?.length);
+            operations?.forEach((op) =>
+                console.log({
+                    id: op.operationId,
+                    call: op.operation?.content?.op?.callSc,
+                })
+            );
+        });
+        stream.on("error", (err) => {
+            console.log(err);
+            reject(err);
+        });
+        stream.on("end", (e: any) => {
+            console.log("end");
+            resolve(e);
+        });
+    });
+};
+
+const subscribe = async <Req, Res extends { toObject: () => any }>(
+    stream: ClientDuplexStream<Req, Res>
+) => {
+    return new Promise((resolve, reject) => {
+        stream.on("data", (data: Res) => {
             console.log(data.toObject());
         });
         stream.on("error", (err) => {
@@ -62,11 +121,12 @@ const subscribe = async () => {
 };
 
 (async () => {
-    const version = await getVersion();
-    console.log(version.getVersion());
+    getVersion().then((v) => console.log(v.getVersion()));
 
-    // const blocks = await getBlocks();
-    // console.log(blocks.toObject());
+    subscribeFilledBlocks();
+    // subscribeNewOperations();
+    // subscribeTransactionsThroughput();
+    // subscribeNewSlotExecutionOutputs();
 
-    subscribe();
+    // subscribe(service.newFilledBlocks());
 })();
